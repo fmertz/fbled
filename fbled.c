c15e0f7aa8c87a54806b817b484c019b4afb8fdb
Francois C Mertz <fcm@DebianDev.localdomain>
2011-03-23 12:37:21 -0400

    Initial version
    This is the initial version of fbled. Only the Load and Traffic LEDs are
    truely supported.

---
+++ fbled.c
@@ -0,0 +1,369 @@
+///           // fbled.c
+//
+// Copyright (C) 2011 - Francois C Mertz
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//  Wed March 16
+//  Copyright  2011  Francois C Mertz
+//  <fcm@DebianDev>
+//
+// This project aims at providing a simple deamon to update the front LEDs on a Watchguard Firebox III. This code is
+// logically separated into a Driver and a Client.
+//
+// The Driver is responsible for updating the LEDs based on input parameters, and deals with low-level I/O ports.
+// This driver might be reimplemented as a real Linux driver/module at a later point.
+//
+// The Client is responsible for gathering the live values from the running system, parse them, normalize them, then pass them
+// to the Driver.
+//=============================================================================
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/io.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+
+//#define LED_DEBUG
+#include "fbled.h"
+
+//This array holds all LED worker functions to be threaded separately
+//
+static tExecTable tLEDProcTable[] = {
+	{ .pfCode=	DoLoad,                 .lWaitBefore_sec=	0L, .lWaitBefore_nsec=	0L, .lWaitAfter_sec=	0L, .lWaitAfter_nsec=	1000000000L/3 },
+	{					DoTraffic,												0L, 									0L, 							0L, 								1000000000L/3 },
+	{					DoBlink,												0L,									0L,							0L,								1000000000L/2 }
+};
+
+//This MutEx is used to serialize access to the LEDs across threads
+//
+static pthread_mutex_t tLEDMutex;
+
+//This is used to control the main loop of threads
+// The worker LED threads will read it, the signal handler can change it
+// Turn-off any optimization by the compiler by declaring it volatile
+//
+static unsigned volatile uKeepGoing=1;
+
+//DRIVER: Initialize function
+//
+static int DrvInit(unsigned char uMode)
+{
+	int 				iRetVal;
+	unsigned	u;
+
+	//In order to make direct use of I/O ports from user space, access has to be requested
+	// Tutorial: http://www.faqs.org/docs/Linux-mini/IO-Port-Programming.html
+	// This needs "root" permission to succeed, and might be Linux-specific
+	iRetVal = IOPERM(LED_BASEPORT, 3, 255); //LINUX
+	if (iRetVal) return iRetVal;
+
+	//Reset all LEDs
+	DrvSetLeds(0x0900);
+	DrvSetLeds(0x0B00);
+	DrvSetLeds(0x0300);
+	DrvSetLeds(0x0700);
+	DrvSetLeds(0x0F00);
+
+	if (DRV_SLOW == uMode)
+	{
+		//Init time animation
+		//One LED at a time for Status LEDs
+		DrvSetLedsWait(LED_DISARMED,DRV_INIT_WAIT);
+		DrvSetLedsWait(LED_ARMED,DRV_INIT_WAIT);
+		DrvSetLedsWait(LED_SYS_A,DRV_INIT_WAIT);
+		DrvSetLedsWait(LED_SYS_B,DRV_INIT_WAIT);
+		DrvSetLeds(0x0900);
+		//One LED at a time for Load LEDs
+		for (u=1; u<256;u*=2)
+			DrvSetLedsWait ((LED_LOAD_LO-1)|u,DRV_INIT_WAIT);
+		DrvSetLeds(0x0B00);
+		//One LED at a time for Traffic LEDs
+		for (u=1; u<256;u*=2)
+			DrvSetLedsWait ((LED_TRAFFIC_LO-1)|u,DRV_INIT_WAIT);
+		DrvSetLeds(0x0300);
+		//Triangle
+		DrvSetLedsWait(LED_TRUST | LED_EXTRN | LED_OPTNL,DRV_INIT_WAIT);
+		DrvSetLeds(LED_T2E_1 | LED_O2E_1);
+		DrvSetLedsWait(LED_E2T_1 | LED_O2T_1| LED_T2O_1 | LED_E2O_1,DRV_INIT_WAIT);
+		//Triangle
+		DrvSetLeds(LED_T2E_2 | LED_O2E_2);
+		DrvSetLedsWait(LED_E2T_2 | LED_O2T_2| LED_T2O_2 | LED_E2O_2,DRV_INIT_WAIT);
+		DrvSetLeds(0x0F00);
+	}
+	//Initialize MutEx for serial access to LEDs from all threads
+	pthread_mutex_init(&tLEDMutex, NULL);
+
+	return 0;
+}
+
+//DRIVER: Finalize function
+//
+static int DrvEnd(void)
+{
+	//Reset all LEDs
+	DrvSetLeds(0x0900);
+	DrvSetLeds(0x0B00);
+	DrvSetLeds(0x0300);
+	DrvSetLeds(0x0700);
+	DrvSetLeds(0x0F00);
+
+	//Cleanup MutEx
+	pthread_mutex_destroy(&tLEDMutex);
+	
+	//This may need "root" access, and is done by Linux anyway on exit
+	// may want to consider dropping this call if we change identity
+	return IOPERM(LED_BASEPORT, 3, 0); //LINUX
+}
+
+//DRIVER: Actually set the LEDs
+//  No buffer, no validation
+//
+static inline void DrvSetLeds(unsigned uCombo)
+{
+	//uCombo is Group# and bits in 2 bytes
+	OUTB((uCombo>>8)&0xFE, LED_CONTROL); //Put Group # in control, Strobe bit to 0
+	OUTB((uCombo&0x00FF)^0xFF, LED_DATA);  //Put data for the LEDs, after inversion (bit 1 means LED is "off")
+	OUTB((uCombo>>8)|0x01, LED_CONTROL); //Same Group #, flipping Strobe bit to 1 to actually set LEDs
+}
+
+//DRIVER: Set LEDs and wait for a while
+//
+static void DrvSetLedsWait(unsigned uCombo, unsigned long ulInterval)
+{
+	const struct timespec tReq = { .tv_sec=0, .tv_nsec=ulInterval};
+	struct timespec tRem = {.tv_sec=0, .tv_nsec=0};
+	
+	DrvSetLeds(uCombo);
+	//Now wait for a while
+	nanosleep(&tReq,&tRem);
+	//Technically, can have time remaining in tRem
+}
+
+//CLIENT: This function makes sure access to LEDs is serialized (thread-safe)
+//
+void SetLeds(unsigned uCombo)
+{
+	//Wait for LED ports to be free, if locked already by another thread
+	pthread_mutex_lock(&tLEDMutex);
+	DrvSetLeds(uCombo);
+	//Release lock
+	pthread_mutex_unlock(&tLEDMutex);
+}
+
+//CLIENT: This worker LED function updates the load LEDs based on system load average
+//
+static void DoLoad(void)
+{
+	static unsigned uLedBitsOld=0;
+	double				dLoadLastMin;
+	unsigned 			uLoad;
+	unsigned 			uLedBits;
+	
+	//Only get 1st number, load avg in last min
+	getloadavg(&dLoadLastMin, 1);
+
+	//Normalize, mapping load to number of LEDs
+	uLoad=(unsigned)(dLoadLastMin*100/15); //This truncates to an integer
+
+	uLoad=(uLoad >= 128) ? 256 : uLoad<<1; //Anything too high will light all LEDs
+
+	for (uLedBits=1;uLoad>1;uLedBits=uLedBits<<1) //Cheap log function
+		uLoad=uLoad>>1;
+
+	//Only update LEDs if necessary
+	if (uLedBits != uLedBitsOld)
+	{
+		uLedBitsOld=uLedBits;
+		SetLeds(LED_LOAD_LO-1+uLedBits-1);
+	}
+}
+
+//CLIENT: This worker LED function updates the Traffic LEDs based on /proc/net/dev
+//
+static void DoTraffic(void)
+{
+	static unsigned 		uLedBitsOld=0;
+	static long long		lAllPacketsPrior=0;
+	static struct timeval	tWhenPrior = { .tv_sec=0, .tv_usec=0};
+	
+	FILE 								*tProcFile=NULL;
+	char							acLineBuffer[2048]="";
+
+	long long 					lRxPackets;
+	long long 					lTxPackets;
+	long long					lAllPackets;
+	long							lRate;
+
+	struct timeval				tWhenNow;
+
+	unsigned 					uLedBits;
+
+	if((tProcFile=fopen( "/proc/net/dev", "r" )) == NULL)
+		return;
+	//Read clock, no timezone
+	if (gettimeofday(&tWhenNow,NULL))
+	{
+		fclose(tProcFile);
+		return;
+	}
+	fgets(acLineBuffer, sizeof(acLineBuffer), tProcFile); //Throw away line 1
+	fgets(acLineBuffer, sizeof(acLineBuffer), tProcFile); //Throw away line 2
+	//Read all lines, 1 by 1
+	for (lAllPackets=0;fgets(acLineBuffer, sizeof(acLineBuffer), tProcFile);)
+         if(    strstr(acLineBuffer, "eth0:") != NULL
+			|| strstr(acLineBuffer, "eth1:") != NULL
+			|| strstr(acLineBuffer, "eth2:") != NULL
+			|| strstr(acLineBuffer, "eth3:") != NULL
+			|| strstr(acLineBuffer, "eth4:") != NULL)
+	    {
+			//Only parse lines for base hardware interfaces, ignore others like lo, bond, tun, VLAN ethx., pppoe
+			//This is done so we avoid counting same packet multiple times
+			//Could be boot time rename of interface eth0-> eth1, could be PCI card with eth4
+            //Spacing is dynamic, sometimes interface name plus ":" is followed by a space, sometimes not
+            if(*( strchr(acLineBuffer,':')+1) == ' ' )
+                sscanf(acLineBuffer, "%*s %*u %Lu %*u %*u %*u %*u %*u %*u %*u %Lu %*u %*u %*u %*u %*u %*u",
+                                  &lRxPackets, &lTxPackets ); //This is for lines like eth1:<space>123
+            else
+                sscanf(acLineBuffer, "%*s %Lu %*u %*u %*u %*u %*u %*u %*u %Lu %*u %*u %*u %*u %*u %*u",
+                                  &lRxPackets, &lTxPackets ); //This is for lines like eth1:123
+			lAllPackets+=lRxPackets+lTxPackets;
+		}
+	fclose(tProcFile);
+	//If first time in, we have neither the old time stamp nor the old packet count
+	if (0L != lAllPacketsPrior)
+	{
+		//Rate is number of packets since last time divided by time diff since last time
+		//Normalize rate by dividing by 64
+		// 64     packets per second is 1 LED
+		lRate=(lAllPackets-lAllPacketsPrior)*1000000/64/((tWhenNow.tv_sec-tWhenPrior.tv_sec)*1000000+tWhenNow.tv_usec-tWhenPrior.tv_usec);
+		// 8192 packets per second and above is 8 LEDs
+		if (lRate > 8192/64)
+			lRate=8192/64;  //Anything higher than 8192 packets/second is all LEDs
+		//compute the logarithm of the rate the cheap way
+		//basically, find the highest bit set to 1
+		for (uLedBits=1;lRate>0;uLedBits=uLedBits<<1) //Cheap log function
+			lRate=lRate>>1;
+		//At this point, uLedBits is a power of 2, and would light only 1 bit
+		//so, by doing uLedBits -1, we set to 1 all bits lower than uLedBits
+		//Only update LEDs if necessary
+		if (uLedBits != uLedBitsOld)
+		{
+			uLedBitsOld=uLedBits;
+			//Use thread-safe LED update
+			SetLeds(LED_TRAFFIC_LO-1+uLedBits-1);
+		}
+	}
+	//Set time and count baseline for next run
+	lAllPacketsPrior=lAllPackets;
+	tWhenPrior=tWhenNow;
+}
+
+//CLIENT: This worker LED function blinks a LED, heartbeat sort of thing
+//
+static void DoBlink(void)
+{
+	static unsigned uStatusLEDs=LED_ARMED | LED_SYS_A;
+
+	//Flip LED_ARMED
+	uStatusLEDs^=LED_ARMED&0x00FF;
+	SetLeds(uStatusLEDs);
+}
+
+//CLIENT: Multithreaded scheduler
+// This function does the infinite loop of wait-run-wait for just one worker
+// This is run by several threads, one for each worker
+//
+void Scheduler(tExecTable *ptSlice)
+{
+	struct timespec tReq = { .tv_sec=0, .tv_nsec=0};
+	struct timespec tRem = {.tv_sec=0, .tv_nsec=0};
+
+	//"infinite" loop, run until global variable changes to 0
+	for (;uKeepGoing;)
+	{
+		//Optional wait before
+		if (ptSlice->lWaitBefore_sec || ptSlice->lWaitBefore_nsec)
+		{
+			tReq.tv_sec=ptSlice->lWaitBefore_sec;
+			tReq.tv_nsec=ptSlice->lWaitBefore_nsec;
+			nanosleep(&tReq,&tRem);
+		}
+		//Worker
+		(*ptSlice->pfCode)();
+		//Optional wait after
+		if (ptSlice->lWaitAfter_sec || ptSlice->lWaitAfter_nsec)
+		{
+			tReq.tv_sec=ptSlice->lWaitAfter_sec;
+			tReq.tv_nsec=ptSlice->lWaitAfter_nsec;
+			nanosleep(&tReq,&tRem);
+		}
+	}
+	//Terminate this thread
+	pthread_exit(NULL);
+}
+
+//CLIENT: This is the handler for all termination signals.
+// Not much can be done here, so just flip a global variable
+// to signal worker threads to stop
+//
+void ExitHandler(int iSigNum)
+{
+	//Just change flag for any signal
+	uKeepGoing=0;
+}
+
+//CLIENT: Entry point upon execution
+//
+int main(int nArgc, char **asArgv)
+{
+	unsigned 						u;
+
+	puts("fbled: Firebox III LED Control Daemon");
+
+	//Initialize Driver, requires "root"
+	if (DrvInit(DRV_FAST))
+	{
+		puts("Cannot Initialize Driver...");
+		return errno;
+	}
+	//Setup handler for all termination signals
+	//These will get called for Control-c, Control-Backslash, or kill
+	signal(SIGINT, ExitHandler);
+	signal(SIGTERM, ExitHandler);
+	signal(SIGQUIT, ExitHandler);
+	//Create threads, by default these threads are joinable
+	for (u=0;u<SIZE(tLEDProcTable);u++)
+		pthread_create(&tLEDProcTable[u].tThread, NULL, (void * (*)(void *))&Scheduler, (void *) &tLEDProcTable[u]);
+
+	//At this point, the main thread continues, in addition to the N worker LED threads
+	//Thread policy: simply choose to wait for the worker threads to complete
+	//It is required that joinable threads be joined in order to free up their resources
+	for (u=0;u<SIZE(tLEDProcTable);u++)
+		pthread_join(tLEDProcTable[u].tThread, NULL); //This is a blocking call, only returning when each worker LED thread terminates
+
+	//At this point, all LED threads have completed
+	
+	//Clean up Driver
+	if (DrvEnd())
+	{
+		puts("Cannot Finalize Driver...");
+		return errno;
+	}
+	return 0;
+}
---
+++ fbled.h
@@ -0,0 +1,190 @@
+//           // fbled.h
+//
+// Copyright (C) 2011 - Francois C Mertz
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//  Wed March 16
+//  Copyright  2011  Francois C Mertz
+//  <fcm@DebianDev>
+//
+// This project aims at providing a simple deamon to update the front LEDs on a Watchguard Firebox III. This code is
+// logically separated into a Driver and a Client.
+//
+// The Driver is responsible for updating the LEDs based on input parameters, and deals with low-level I/O ports.
+// This driver might be reimplemented as a real Linux driver/module at a later point.
+//
+// The Client is responsible for gathering the live values from the running system, parse them, normalize them, then pass them
+// to the Driver.
+//=============================================================================
+//
+// Driver Logic:
+//  The LEDs are controlled similarly to controlling a printer on the good old PC parallel port.
+//  The general idea is that there are a number of on/off lights that need to be controlled individually. This is done by sending
+//  data to specific "ports", 8-bit at a time. As we have more than 8 LEDs, there is the issue of addressing the LEDs and the
+//  issue of controlling them. This mapping has been reverse-engineered by Jani Laaksonen.
+//  The short version is that the data port, BASEPORT+0, controls a group of up to 8 LEDs, one LED for each bit.
+//  What group of 8 LEDs the data is for is controlled by the control port, BASEPORT+2. The actual updating of the LEDs is
+//  a 3 steps process:
+//  1.The control port is updated with the LED Group Number
+//  2.The LED data is placed in the data port, 1 bit for each LED
+//  3.Control bit-0 of the control port is then flipped to actually change the LED
+//
+//  The mapping of LED Group to Control bits is:
+//		Group 0, 0x02->0x03, This is for the 4 Status LEDs
+//		Group 1, 0x00->0x01, This is for 8 Load LEDs
+//		Group 2, 0x08->0x09, This is for 8 Traffic LEDs
+//		Group 3, 0x0c->0x0d, This is for part of the triangle (7 LEDs)
+//		Group 4, 0x04->0x05, This is for the rest of the triangle (8 LEDs)
+//
+//  Last, there is hardware inversion for bits 1011, so these bits need to be flipped.
+//                      See http://en.wikipedia.org/wiki/Parallel_port
+//
+//  Note: LEDs are controlled individually, there is no grouping of any kind, i.e. the 8 LEDs for Traffic or Load can
+//            be lit up individually, there is no "stack" concept in hardware. It all need to be built in the code.
+//  Note: As is tradition, the Driver is just responsible for offering the means to control the LEDs. It is not responsible for
+//            policy, i.e. what information in what form goes to what LED.
+//  Note: This is a Linux implementation.
+//=============================================================================
+//
+// Client Logic:
+//  The LEDs are labelled in a pretty generic way, and can be used to convey a number of statuses. This Client imlements
+//  the policies described below. The original policies from the manufacturer are quoted.
+
+//  Disarm: "Red light indicates the Firebox detected an error, shut down its interfaces, and will not forward any packets. Reboot the Firebox."
+//               Idea: ready to poweroff, root file system unmounted
+//  Armed: "Green light indicates the Firebox has been booted and is running."
+//               Idea: WAN has IP address.
+//               Idea: Firewall has at least 1 entry
+//               Idea: the boot process has completed
+//               Idea: flashes to indictate this code is running
+//    Sys A: "Indicates that the Firebox is running from its primary user-defined configuration."
+//               Idea: os running off of randrive, early boot
+//    Sys B: "Indicates that the Firebox is running from the readonly factory default system area."
+//               Idea: os running off of storage device, root file system mounted, normal operation
+//   Power: "Indicates that the Firebox is currently powered up."
+//               Direct harware wiring, no software control
+// Triangle: "Indicates traffic between Firebox interfaces. Green arrows briefly light to indicate allowed traffic
+//                between two interfaces in the direction of the arrows. A red light at a triangle corner indicates 
+//                that the Firebox is denying packets at that interface."
+//                Idea: for External, Trusted and Other, light corner is interface has an IP address
+//                Idea: for External, Trusted and Other, light corner is interface is up/has a link
+//                Idea: for External, Trusted and Other, light corner if interface has a firewall attached
+//    Traffic: "A stack of lights that functions as a meter to indicate levels of traffic volume through the 
+//                Firebox. Low volume indicators are green, while high volume indicators are yellow. The display 
+//                updates three times per second. The scale is exponential: the first light represents 64 packets/ 
+//                second, the second light represents 128 packets/second, increasing to the eighth light which 
+//                represents 8,192 packets/second. "
+//                This stack is controlled by /proc/net/dev, with a logarithmic scale.
+//     Load: "A stack of lights that functions as a meter to indicate the system load average. The system load 
+//               average is the average number of processes running (not including those in wait states) during 
+//               the last minute. Low average indicators are green, while high average indicators are yellow. The 
+//               display updates three times per second. The scale is exponential with each successive light representing 
+//               a doubling of the load average. The first light represents a load average of 0.15. The most 
+//               significant load factor on a Firebox is the number of proxies running."
+//               This stack is controlled by /proc/loadavg, with a logarithmic scale. The first number is the number of processes
+//               actually using CPU cycles to run during the last minute.
+//
+// Client Architecture
+//               Overall, the client is basically an infinite loop of execution threads with a configurable wait time. The
+//               implementation is based on multi-threading a number of functions (the Do... functions), one per group
+//               of LEDs. The exit condition is the receipt of one of the usual termination signals.
+//=============================================================================
+
+//Driver Constants
+#define LED_BASEPORT	0x378
+#define LED_DATA			(LED_BASEPORT+0)
+#define LED_CONTROL		(LED_BASEPORT+2)
+
+//LED Coding; Control bits in 1 byte followed by Data bits for each LED
+//LED Group 0: Status LEDs, Control Code 2, 0010 xor'ed with 1011 is 1001, 0x09
+#define LED_DISARMED	0x0901
+#define LED_ARMED			0x0902
+#define LED_SYS_A 			0x0904
+#define LED_SYS_B			0x0908
+#define LED_ENABLE		0x0940
+
+//LED Group 1: Load, Control Code 0, 0000 xor'ed with 1011 is 1011, 0x0B
+#define LED_LOAD_LO		0x0B01
+#define LED_LOAD_HI		0x0BFF
+
+//LED Group 2: Traffic, Control Code 8, 1000 xor'ed with 1011 is 0011, 0x03
+#define LED_TRAFFIC_LO	0x0301
+#define LED_TRAFFIC_HI	0x03FF
+     
+//LED Group 3: Triangle, Control Code C, 1100 xor'ed with 1011 is 0111, 0x07
+#define LED_T2E_1			0x0701
+#define LED_T2E_2			0x0702
+#define LED_O2E_1			0x0704
+#define LED_O2E_2			0x0708
+#define LED_EXTRN			0x0710
+#define LED_TRUST			0x0720
+#define LED_OPTNL			0x0740
+
+//LED Group 4: Triangle, Control Code 4, 0100 xor'ed with 1011 is 1111, 0x0F
+#define LED_E2T_1			0x0F01
+#define LED_E2T_2			0x0F02
+#define LED_O2T_1			0x0F04
+#define LED_O2T_2			0x0F08
+#define LED_T2O_1			0x0F10
+#define LED_T2O_2			0x0F20
+#define LED_E2O_1			0x0F40
+#define LED_E2O_2			0x0F80
+
+//Driver constants
+#define DRV_FAST			0
+#define DRV_SLOW			1
+#define DRV_INIT_WAIT		200000000 //.2 sec
+
+//Driver prototypes
+static int DrvInit(unsigned char);
+static int DrvEnd(void);
+static inline void DrvSetLeds(unsigned);
+static void DrvSetLedsWait(unsigned, unsigned long);
+
+//Client Structures
+struct ExecTable
+{
+	void 		(*pfCode)(void);					    //Code to run
+	long		lWaitBefore_sec;						//How long before 1st run
+	long		lWaitBefore_nsec;
+	long		lWaitAfter_sec;							//How long to wait after run
+	long		lWaitAfter_nsec;
+	pthread_t tThread;
+};
+
+typedef struct ExecTable tExecTable;
+
+//Client Contants
+
+//Client Prototypes
+static void DoLoad(void);
+static void DoTraffic(void);
+static void DoBlink(void);
+void Scheduler(tExecTable *);
+static void SetLeds(unsigned);
+void ExitHandler(int);
+
+//Debugging Assist
+#ifndef LED_DEBUG
+//Run-time version of low-level io routines
+#define OUTB(port,val) outb((port),(val))
+#define IOPERM(port, count, yesno) ioperm((port), (count), (yesno))
+#else
+//Debug version of low-level routines
+#define OUTB(val,port) printf("outb(%04X,%02X)\n",port,val)
+#define IOPERM(port, count, yesno) (printf("ioperm(%04X,%u)\n",port,count)==0)
+#endif
+
+#define SIZE(a) sizeof(a)/sizeof(a[0])
